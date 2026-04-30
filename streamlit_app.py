7d7a196afd2a802e76c34c9c71b4d37022024f3f
samwil22 <110462840+samwil22@users.noreply.github.com>
2026-05-01 01:29:58 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 #from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import cosine_similarity
 
 # =========================
 # CONFIG
